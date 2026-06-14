251bb7e214c1c5310df542f78a61af14304b197f
Saleh Ahammed Noor <salehahammednoor@gmail.com>
2026-06-14 23:47:10 +0600

    feat: add configuration publishing support

--- src/Providers/ShopAssistServiceProvider.php
+++ src/Providers/ShopAssistServiceProvider.php
@@ -8,11 +8,15 @@ class ShopAssistServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        //
+        $this->mergeConfigFrom(
+            __DIR__ . '/../../config/shopassist-ai.php', 'shopassist-ai'
+        );
     }
 
     public function boot(): void
     {
-        //
+        $this->publishes([
+           __DIR__ . '/../../config/shopassist-ai.php' => config_path('shopassist-ai.php'),
+        ], 'shopassist-config');
     }
 }
