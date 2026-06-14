ec6aa2ff8750bcb0831dae5ae5c9d77a74feb7b4
Saleh Ahammed Noor <salehahammednoor@gmail.com>
2026-06-14 19:42:29 +0600

    Initial setup is done

---
+++ src/Providers/ShopAssistServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace SanRatul\ShopAssist\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ShopAssistServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        //
+    }
+
+    public function boot(): void
+    {
+        //
+    }
+}
